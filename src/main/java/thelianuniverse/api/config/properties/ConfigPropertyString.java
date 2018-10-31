1a67235926e8dc4280d49889dfc918bc4e0c6bed
EmberFlash <thelianinteractive@gmail.com>
2018-10-30 19:37:07 -0600

    Mod Configs
    Created Config Properties and Framework, Added Default Config File.

---
+++ src/main/java/thelianuniverse/api/config/properties/ConfigPropertyString.java
@@ -0,0 +1,23 @@
+package thelianuniverse.api.config.properties;
+
+public class ConfigPropertyString extends ConfigProperty {
+
+    public String valueString;
+
+    public ConfigPropertyString(String name, String category, String description, String defaultValue) {
+
+        super(Type.STRING, name, category, description);
+
+        this.valueString = defaultValue;
+
+        this.formatDescription();
+    }
+
+    public String get() {
+        return this.valueString;
+    }
+
+    public void set(String value) {
+        this.valueString = value;
+    }
+}
