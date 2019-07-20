f6afcc3ce18ad2a32a4bdaa36a29ee4277440c8c
EmberFlash <thelianinteractive@gmail.com>
2019-07-20 14:32:36 -0600

    Updating MDK to 28.0.2
    Initialize Port for MC 1.14.4

--- src/main/java/thelianuniverse/thelianuniverse.java
+++ src/main/java/thelianuniverse/thelianuniverse.java
@@ -15,71 +15,59 @@ import static thelianuniverse.thelianuniverse.*;
 import thelianuniverse.main.proxy.ClientProxy;
 import thelianuniverse.main.proxy.CommonProxy;
 
+
 @Mod(
-        modid = MOD_ID,
-        name =  MOD_NAME,
-        version = MOD_VERSION,
-        acceptableRemoteVersions = "*"
+		modid = MOD_ID,
+		name = MOD_NAME,
+		version = MOD_VERSION,
+		acceptableRemoteVersions = "*"
 )
 
 public final class thelianuniverse {
-
-    public static final String MOD_ID = Thelian_ID;
-    public static final String MOD_NAME = "ThelianUniverse";
-    public static final String MOD_VERSION = "@MOD_VERSION@";
-    private static final thelianuniverse instance = new thelianuniverse();
-    @SidedProxy(serverSide = CommonProxy.LOCATION, clientSide = ClientProxy.LOCATION)
-    public static CommonProxy proxy;
-
-    private thelianuniverse() {
-
-    }
-
-    @Mod.InstanceFactory
-    public static thelianuniverse getInstance(){
-
-        return instance;
-    }
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-
-        proxy.preInit(event);
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-
-        proxy.init(event);
-
-    }
-
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-
-        proxy.postInit(event);
-    }
-    @EventHandler
-    public void loadComplete(FMLLoadCompleteEvent event) {
-
-        proxy.loadComplete();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+				
+				public static final String MOD_ID = Thelian_ID;
+				public static final String MOD_NAME = "ThelianUniverse";
+				public static final String MOD_VERSION = "@MOD_VERSION@";
+				private static final thelianuniverse instance = new thelianuniverse();
+				@SidedProxy(serverSide = CommonProxy.LOCATION, clientSide = ClientProxy.LOCATION)
+				public static CommonProxy proxy;
+				
+				private thelianuniverse() {
+				
+				}
+				
+				@Mod.InstanceFactory
+				public static thelianuniverse getInstance() {
+								
+								return instance;
+				}
+				
+				@EventHandler
+				public void preInit(FMLPreInitializationEvent event) {
+								
+								proxy.preInit(event);
+				}
+				
+				@EventHandler
+				public void init(FMLInitializationEvent event) {
+								
+								proxy.init(event);
+								
+				}
+				
+				@EventHandler
+				public void postInit(FMLPostInitializationEvent event) {
+								
+								proxy.postInit(event);
+				}
+				
+				@EventHandler
+				public void loadComplete(FMLLoadCompleteEvent event) {
+								
+								proxy.loadComplete();
+				}
+				
+				
 }
 
 
